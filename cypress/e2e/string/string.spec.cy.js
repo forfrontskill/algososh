bf27f7acb7c7679074f10f3b7d945e151632e796
Alexey <avmakarov.mail@yandex.ru>
2023-03-28 00:48:30 +0300

    Добавление тестов Cypress для разворота строки и фибоначи

--- cypress/e2e/string/string.spec.cy.js
+++ cypress/e2e/string/string.spec.cy.js
@@ -1,14 +1,44 @@
+
 describe('app works correctly with routes', function () {
     beforeEach(function () {
         cy.visit('http://localhost:3000/recursion');
     });
 
-    it('should open home page', function () {
+    it('Check button logic', function () {
         cy.contains('Строка');
         cy.get('[data-testid="input"]').type('hello');
         cy.get('button').should('not.be.disabled');
+        cy.wait(3000);
         cy.get('[data-testid="input"]').clear();
         cy.get('button').should('be.disabled');
     });
+
+    it('Check visual', function () {
+        const word1 = 'hello';
+        const styles1 = ['default','default','default','default','default']
+        cy.get('[data-testid="input"]').type(word1);
+        cy.get('button').contains('Развернуть').click();
+        cy.get('[data-testid="circle-main"]').each(($el, index, $list)=>{
+            cy.wrap($el).contains(word1[index]);
+            cy.wrap($el).invoke('attr','class').should('contain', styles1[index]);
+        })
+
+        const word2 = 'oellh';
+        const styles2 = ['changing','default','default','default','changing'];
+
+        cy.get('[data-testid="circle-main"]').each(($el, index, $list)=>{
+            cy.wrap($el).contains(word2[index]);
+            cy.wrap($el).invoke('attr','class').should('contain', styles2[index]);
+        });
+
+        const word3 = 'oellh';
+        const styles3 = ['changing','default','default','default','changing'];
+
+        cy.get('[data-testid="circle-main"]').each(($el, index, $list)=>{
+            cy.wrap($el).contains(word3[index]);
+            cy.wrap($el).invoke('attr','class').should('contain', styles3[index]);
+        })
+    });
+
 
 }); 
